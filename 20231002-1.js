be404b9fe8cb201ef114a934ae5ccb02ced3bc8e
AChang0402 <alexchang.0402@gmail.com>
2023-10-02 23:55:11 -0700

    2023.10.02

---
+++ 20231002-1.js
@@ -0,0 +1,25 @@
+// 1512. Number of Good Pairs
+
+function numIdenticalPairs (nums){
+    let seen = {}
+    for (let i=0; i<nums.length; i++){
+        if (nums[i] in seen){
+            seen[nums[i]]++
+        }
+        else{
+            seen[nums[i]]=1;
+        }
+    }
+    let totalPairs = 0
+    let pairsToAdd = 0
+    for (let key in seen){
+        if (seen[key]>1){
+            pairsToAdd = (seen[key]*(seen[key]-1))/2
+            totalPairs += pairsToAdd
+        }
+    }
+    return totalPairs
+}
+
+let nums1 = [1,2,3,1,1,3]
+console.log(numIdenticalPairs(nums1))
