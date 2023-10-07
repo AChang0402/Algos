fe116de4e4c06b9baa172b628950dc51d0697ba3
AChang0402 <alexchang.0402@gmail.com>
2023-10-07 00:18:59 -0700

    2023.10.06-1.2

--- 20231006-1.js
+++ 20231006-1.js
@@ -1,42 +1,45 @@
 // 2. Add Two Numbers
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
+
 var addTwoNumbers = function(l1, l2) {
     let runner1 = l1
     let int1 = 0
     let count = 0
     let runner2 = l2
     let int2= 0
-    while (runner1 || runner2){
-        if(runner1){
-            int1 += runner1.val*(10**count)
-            runner1 = runner1.next
-        }
-        if(runner2){
-            int2 += runner2.val*(10**count)
-            runner2 = runner2.next
-        }
+    // while (runner1 !== null || runner2 !== null){
+    //     if(runner1 !== null){
+    //         int1 += runner1.val*(10**count)
+    //         runner1 = runner1.next
+    //     }
+    //     if(runner2 !== null){
+    //         int2 += runner2.val*(10**count)
+    //         runner2 = runner2.next
+    //     }
+    //     count++
+    // }
+    while (runner1 !== null){
+        int1 += runner1.val*(10**count)
+        runner1 = runner1.next
         count++
     }
+
+    count=0
+    while (runner2 !== null){
+        int2 += runner2.val*(10**count)
+        runner2 = runner2.next
+        count++
+    }
+
     let sum = int1+int2
     let answer = new ListNode(sum%10)
     sum = Math.floor(sum/10)
     let runner3 = answer
     while (sum>0){
-        runner3.next = newListNode(sum%10)
+        runner3.next = new ListNode(sum%10)
         sum = Math.floor(sum/10)
         runner3 = runner3.next
     }
     return answer
 };
+
