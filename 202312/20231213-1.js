86bbf648b60f5808219ee8c402a73c5a3cb35e46
AChang0402 <alexchang.0402@gmail.com>
2023-12-13 22:11:41 -0800

    2023.12.13

---
+++ 202312/20231213-1.js
@@ -0,0 +1,43 @@
+
+function moveZeroes(nums){
+    let i=0;
+    let j=i+1;
+    while (j<nums.length){
+        if(nums[i]!==0){
+            i++
+            j++
+        }
+        else{
+            if(nums[j]!==0){
+                [nums[i],nums[j]]=[nums[j],nums[i]]
+                i++
+            }
+            j++
+        }
+    }
+}
+
+
+//first attempt: slow
+// function moveZeroes(nums){
+//     let x=0
+//     let y=nums.length-1
+//     while (x<y){
+//         if (nums[x]==0){
+//             let i=x
+//             while(i<y){
+//                 [nums[i],nums[i+1]] = [nums[i+1],nums[i]]
+//                 i++
+//             }
+//             y--
+//         }
+//         else{
+//             x++
+//         }
+//     }
+// }
+
+let nums1 = [0,0,1]
+moveZeroes(nums1)
+let nums2 = [0,1,0,3,12]
+moveZeroes(nums2)
