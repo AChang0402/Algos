27884bfdc9ec180abfaa9fa87a716871165dd50c
AChang0402 <alexchang.0402@gmail.com>
2023-12-20 22:22:57 -0800

    2023.12.20

--- 202312/20231220-1.js
+++ 202312/20231220-1.js
@@ -1,18 +1,37 @@
 // 1456. Maximum Number of Vowels in a Substring of Given Length
 
-function maxVowels(s,k){
-    let max = 0;
-    for (let i=0; i<s.length-k+1; i++){
-        let count = 0;
-        for(let j=i; j<i+k; j++){
-            if(s[j]=="A" || s[j]=="a" || s[j]=="E" || s[j]=="e" || s[j]=="I" || s[j]=="i" || s[j]=="O" || s[j]=="o" || s[j]=="U" || s[j]=="u"){
-                count++
-            }
+// function maxVowels(s,k){
+//     let max = 0;
+//     for (let i=0; i<s.length-k+1; i++){
+//         let count = 0;
+//         for(let j=i; j<i+k; j++){
+//             if(s[j]=="A" || s[j]=="a" || s[j]=="E" || s[j]=="e" || s[j]=="I" || s[j]=="i" || s[j]=="O" || s[j]=="o" || s[j]=="U" || s[j]=="u"){
+//                 count++
+//             }
+//         }
+//         max = Math.max(max, count)
+//     }
+//     return max
+// }
+
+function maxVowels(s, k){
+    let maxCount = 0
+    let count = 0
+    for (let i=0; i<s.length; i++){
+        if(s[i]=="a" || s[i]=="e" || s[i]=="i" || s[i]=="o" || s[i]=="u"){
+            count++
         }
-        max = Math.max(max, count)
+        if(s[i-k]=="a" || s[i-k]=="e" || s[i-k]=="i" || s[i-k]=="o" || s[i-k]=="u"){
+            count--
+        }
+        if(count == k){
+            return k
+        }
+        maxCount = Math.max(count, maxCount)
     }
-    return max
+    return maxCount
 }
+
 
 let s1 = "weallloveyou"
 let k1 = 7
